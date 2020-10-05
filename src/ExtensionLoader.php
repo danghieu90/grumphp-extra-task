03619969c06c06fc4335eea29f1402e0f4a3360a
Hieu Dang <danghieu14@gmail.com>
2020-10-05 21:47:12 +0700

    add phpmd

--- src/ExtensionLoader.php
+++ src/ExtensionLoader.php
@@ -6,6 +6,7 @@ use GrumPHP\Extension\ExtensionInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 use HD\GrumPhpExtraTask\Task\PhpCs;
+use HD\GrumPhpExtraTask\Task\PhpMd;
 
 class ExtensionLoader implements ExtensionInterface
 {
@@ -15,5 +16,10 @@ class ExtensionLoader implements ExtensionInterface
             ->addArgument(new Reference('process_builder'))
             ->addArgument(new Reference('formatter.phpcs'))
             ->addTag('grumphp.task', ['task' => 'php_cs']);
+
+        $container->register('task.php_md', PhpMd::class)
+            ->addArgument(new Reference('process_builder'))
+            ->addArgument(new Reference('formatter.raw_process'))
+            ->addTag('grumphp.task', ['task' => 'php_md']);
     }
 }
