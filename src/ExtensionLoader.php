765030f45ac205310f7fdd9936340532aaa675c7
Hieu Dang <danghieu14@gmail.com>
2020-10-15 01:14:26 +0700

    add phpstan

--- src/ExtensionLoader.php
+++ src/ExtensionLoader.php
@@ -5,40 +5,40 @@ namespace HD\GrumPhpExtraTask;
 use GrumPHP\Extension\ExtensionInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
-use HD\GrumPhpExtraTask\Task\PhpCs;
-use HD\GrumPhpExtraTask\Task\EsLint;
-use HD\GrumPhpExtraTask\Task\Jscs;
-use HD\GrumPhpExtraTask\Task\Composer;
-use HD\GrumPhpExtraTask\Task\PhpCompatibility;
 
 class ExtensionLoader implements ExtensionInterface
 {
     public function load(ContainerBuilder $container): void
     {
-        $container->register('task.php_cs', PhpCs::class)
+        $container->register('task.php_cs', Task\PhpCs::class)
             ->addArgument(new Reference('process_builder'))
             ->addArgument(new Reference('formatter.phpcs'))
             ->addTag('grumphp.task', ['task' => 'php_cs']);
 
-        $container->register('task.es_lint', EsLint::class)
+        $container->register('task.es_lint', Task\EsLint::class)
             ->addArgument(new Reference('process_builder'))
             ->addArgument(new Reference('formatter.raw_process'))
             ->addTag('grumphp.task', ['task' => 'es_lint']);
 
-        $container->register('task.jscs', Jscs::class)
+        $container->register('task.jscs', Task\Jscs::class)
             ->addArgument(new Reference('process_builder'))
             ->addArgument(new Reference('formatter.raw_process'))
             ->addTag('grumphp.task', ['task' => 'jscs']);
 
-        $container->register('task.composer_validate', Composer::class)
+        $container->register('task.composer_validate', Task\Composer::class)
             ->addArgument(new Reference('process_builder'))
             ->addArgument(new Reference('formatter.raw_process'))
             ->addArgument(new Reference('grumphp.util.filesystem'))
             ->addTag('grumphp.task', ['task' => 'composer_validate']);
 
-        $container->register('task.php_compatibility', PhpCompatibility::class)
+        $container->register('task.php_compatibility', Task\PhpCompatibility::class)
             ->addArgument(new Reference('process_builder'))
             ->addArgument(new Reference('formatter.phpcs'))
             ->addTag('grumphp.task', ['task' => 'php_compatibility']);
+
+        $container->register('task.php_stan', Task\PhpStan::class)
+            ->addArgument(new Reference('process_builder'))
+            ->addArgument(new Reference('formatter.raw_process'))
+            ->addTag('grumphp.task', ['task' => 'php_stan']);
     }
 }
